e7f4ba84595e61911c8a00dec5fb9229f71a78fc
Sithira Selaka <sithira.ariyapperuma@gmail.com>
2025-03-01 22:20:57 +0530

    feat: set up feature flags

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -15,6 +15,7 @@ import { draftMode } from 'next/headers'
 
 import './globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
+import { PostHogProvider } from './providers'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
@@ -28,15 +29,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       </head>
       <body>
         <Providers>
-          <AdminBar
-            adminBarProps={{
-              preview: isEnabled,
-            }}
-          />
+          <PostHogProvider>
+            <AdminBar
+              adminBarProps={{
+                preview: isEnabled,
+              }}
+            />
 
-          <Header />
-          {children}
-          <Footer />
+            <Header />
+            {children}
+            <Footer />
+          </PostHogProvider>
         </Providers>
       </body>
     </html>
